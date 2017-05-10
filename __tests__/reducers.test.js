b35b8fdd289839a572e2ea74c2c98bbbc5cf7ba6
Sami Keinänen <skope@iki.fi>
2017-05-10 17:32:32 +0300

    Improved reducer testing

--- __tests__/reducers.test.js
+++ __tests__/reducers.test.js
@@ -1,3 +1,5 @@
+import EXAMPLE_ACTION_TYPE from '../src/constants/ActionTypes';
+
 import reducers from '../src/reducers';
 
 describe('reducers', () => {
@@ -5,6 +7,17 @@ describe('reducers', () => {
     it('should return the initial state', () => {
       expect(reducers.example(undefined, {})).toEqual({
         example: 'foo'
+      });
+    });
+
+    it('should return changed state', () => {
+      const value = 'bar';
+
+      expect(reducers.example(undefined, {
+        type: EXAMPLE_ACTION_TYPE,
+        example: value
+      })).toEqual({
+        example: value
       });
     });
   });
