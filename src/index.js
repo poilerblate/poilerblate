067611b604b60d7598b08f277b53d79fc8b7af55
Sami Keinänen <skope@iki.fi>
2017-05-10 16:09:58 +0300

    Moved reducer combination to react index page

--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 
 import reducers from './reducers';
 
-import App from './components/App';
+import Container from './components/App';
 
-let store = createStore(reducers);
+let store = createStore(
+  combineReducers(reducers)
+);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Container />
   </Provider>,
   document.getElementById('root')
 );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,5 @@
-import { combineReducers } from 'redux';
 import example from './example';
 
-export default combineReducers({
+export default {
   example
-});
+};
