ab75ec524a5a26d94dc74be8c3810f27fed00a93
Sami Keinänen <skope@iki.fi>
2017-10-07 22:00:24 +0300

    Updated npm packages and made according changes to tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
 import { App } from 'components/App';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('App component', () => {
   it('should contain a wrapper div with a classname', () => {
--- src/components/NotFound/NotFound.test.js
+++ src/components/NotFound/NotFound.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
 import { NotFound } from 'components/NotFound';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('NotFound component', () => {
   it('should contain a wrapper div with a classname', () => {
