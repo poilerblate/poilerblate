4e6a802b4a1253fc7355c897e96eff89a0d543d6
Sami Keinänen <skope@iki.fi>
2017-10-25 19:32:07 +0300

    Composing redux devtools into the store broke the app if extension wasn't installed

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ import { Provider } from 'react-redux';
 import {
   createStore,
   combineReducers,
-  applyMiddleware,
-  compose
+  applyMiddleware
 } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
 import reducers from 'reducers';
@@ -23,10 +23,7 @@ import NotFoundComponent from 'components/NotFound';
 
 let store = createStore(
   combineReducers(reducers),
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeWithDevTools(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
