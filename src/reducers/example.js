68af9838ed8fd0b41628856f3e2e46eae597843b
Juha Tauriainen <juha.tauriainen@houston-inc.com>
2019-10-14 14:42:38 +0300

    Format js files according to eslint

--- src/reducers/example.js
+++ src/reducers/example.js
@@ -1,8 +1,11 @@
 import * as types from 'constants/ActionTypes';
 
-export default function example(state = {
-  example: 'foo',
-}, action) {
+export default function example(
+  state = {
+    example: 'foo',
+  },
+  action,
+) {
   if (types.CHANGE_EXAMPLE_ACTION_TYPE === action.type) {
     return { ...state, example: action.example };
   }
